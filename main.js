2538a8a1ce096983e03642a5bebcc5a95cad1f10
moshe212 <moshe212@gmail.com>
2021-11-02 16:10:27 +0200

    ג

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ client.on("authenticated", (session) => {
 // });
 client.initialize();
 
-const job1 = schedule.scheduleJob("15 * * * *", bed24Func.getDeparture);
+const job1 = schedule.scheduleJob("17 * * * *", bed24Func.getDeparture);
 // const job2 = schedule.scheduleJob("59 * * * *", bed24Func.getArrival);
 
 app.get("*", (req, res) => {
