46b5935eda1437b5d84a3bb2bbaa71dde436c581
moshe212 <moshe212@gmail.com>
2021-11-12 13:53:11 +0200

    ד

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const fs = require("fs");
 
 dotenv.config();
 // app.use(bodyParser.json());
-app.use(express.json());
+app.use(express.json({ limit: "1mb" }));
 app.use(upload.array());
 app.use(express.static("public"));
 
