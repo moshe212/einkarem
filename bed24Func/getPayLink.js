36cc63170868e8b7dccceb007ec6f080811963d5
moshe212 <moshe212@gmail.com>
2021-11-15 21:26:26 +0200

    d

--- bed24Func/getPayLink.js
+++ bed24Func/getPayLink.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+var request = require("request");
 
 const getPayLink = async (
   pageCode,
@@ -15,18 +16,37 @@ const getPayLink = async (
   let PayUrl = "";
   const url = `https://sandbox.meshulam.co.il/api/light/server/1.0/createPaymentProcess/?pageCode=${pageCode}&userId=${userId}&apiKey=&sum=${sum}&successUrl=${successUrl}&cancelUrl=${cancelUrl}&description=${description}&paymentNum=&maxPaymentNum=1&pageField[fullName]=${fullName}&pageField[phone]=${phone}&cField1=${bookId}&cField2=${place}`;
   console.log("url", url);
-  await axios({
-    method: "post",
-    url: url,
-    headers: {
-      "Content-Type": "application/json",
+  request(
+    {
+      method: "POST",
+      url: url,
+      headers: {
+        "Content-Type": "application/json",
+      },
     },
-  }).then(function (response) {
-    console.log("Status:", response.statusCode);
-    console.log("Headers:", JSON.stringify(response.headers));
-    console.log("Response:", response.data.data.url);
-    PayUrl = response.data.data.url;
-  });
+    function (error, response, body) {
+      if (error) {
+        return console.error(error);
+      }
+      console.log("Status:", response.statusCode);
+      //   console.log("Headers:", JSON.stringify(response.headers));
+      console.log("Response:", body);
+      console.log("Response:", body.data.data.url);
+      PayUrl = body.data.data.url;
+    }
+  );
+  //   await axios({
+  //     method: "post",
+  //     url: url,
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   }).then(function (response) {
+  //     console.log("Status:", response.statusCode);
+  //     console.log("Headers:", JSON.stringify(response.headers));
+  //     console.log("Response:", response.data.data.url);
+  //     PayUrl = response.data.data.url;
+  //   });
   return PayUrl;
 };
 
