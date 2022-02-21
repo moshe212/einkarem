20dd2d417c333ced40e51b9ecb66af8adddbc977
moshe212 <moshe212@gmail.com>
2022-02-21 13:32:51 +0200

    s

--- main.js
+++ main.js
@@ -82,7 +82,7 @@ const craeteStageFile = async () => {
   stagesData = await JSON.parse(data);
   console.log("stagesData", stagesData);
 };
-const job1 = schedule.scheduleJob("40 07 * * 0-6", craeteStageFile);
+const job1 = schedule.scheduleJob("40 11 * * 0-6", craeteStageFile);
 craeteStageFile();
 // const sendCheckInOut = async () => {
 //   await bed24Func.getArrival();
